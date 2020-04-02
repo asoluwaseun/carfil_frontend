1e2b1e9e2207c742a729c2b5103bc6a8aeb0cd71
asoluwaseun <soadesina71@student.lautech.edu.ng>
2020-04-02 10:50:43 +0100

    Updated the width of the content area of the pages

--- src/pages/CarOwners.jsx
+++ src/pages/CarOwners.jsx
@@ -32,85 +32,95 @@ export default class CarOwners extends Component {
     render() {
         return (
             <div className="container py-3">
-                {
-                    this.state.loading &&
-                    (
-                        <div className="text-center p-5">
-                            <i className="fa fa-spin fa-spinner fa-2x"></i>
-                            <br/>
-                            <small>Loading</small>
-                        </div>
-                    )
-                }
-                {
-                    !this.state.loading && this.state.car_owners.length === 0 &&
-                    (
-                        <div className="text-center p-5">
-                            <small>No Car Owner Found Based On Chosen Filter</small>
-                            <br/>
-                            <Link to="/">Go Back</Link>
-                        </div>
-                    )
-                }
-                {
-                    this.state.car_owners.map((car_owner) => {
-                        return (
-                            <div key={car_owner.id} className="card shadow-lg mb-3">
-                                <div className="card-body">
-                                    <div className="row">
-                                        <div className="col-4">
-                                            <img src={car} width="120" height="120" className="img-responsive"/>
-                                        </div>
-                                        <div className="col-8">
-                                            <h6>{car_owner.first_name + " " + car_owner.last_name}</h6>
-                                            <div className="">
-                                                <ul className="p-0">
-                                                    <li className="border-right p-1">
-                                                        <p className="mb-0 text-muted c-text">Brand</p>
-                                                        <p className="mb-0 c-text">{car_owner.car_model}</p>
-                                                    </li>
-                                                    <li className="border-right p-1">
-                                                        <p className="mb-0 text-muted c-text">Year</p>
-                                                        <p className="mb-0 c-text">{car_owner.car_model_year}</p>
-                                                    </li>
-                                                    <li className="p-1 text-center">
-                                                        <p className="mb-0 text-muted c-text">Color</p>
-                                                        <p className="mb-0 color text-center" style={{margin: 'auto', backgroundColor: car_owner.car_color}}></p>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <div className="">
-                                                <ul className="p-0">
-                                                    <li className="p-1">
-                                                        <p className="mb-0 text-muted c-text">Country</p>
-                                                        <p className="mb-0 c-text">{car_owner.country}</p>
-                                                    </li>
-                                                    <li className=" p-1">
-                                                        <p className="mb-0 text-muted c-text">Gender</p>
-                                                        <p className="mb-0 c-text">{car_owner.gender}</p>
-                                                    </li>
-                                                    <li className="p-1">
-                                                        <p className="mb-0 text-muted c-text">Job</p>
-                                                        <p className="mb-0 c-text bio">{car_owner.job_title}</p>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <div className="ml-1">
-                                                <p className="c-text">
-                                                    <span className="text-muted">Email:</span> {car_owner.email}
-                                                </p>
-                                                <p className="c-text bio">
-                                        <span
-                                            className="text-muted">Bio:</span> {car_owner.bio}
-                                                </p>
+                <div className="row">
+                    <div className="col-md-2 col-lg-2"></div>
+                    <div className="col-md-8 col-lg-8">
+                        <Link to="/">
+                            <h6 className="p-2"><i className="zmdi zmdi-arrow-left"></i> Home</h6>
+                        </Link>
+                        <h2 className="p-2 mt-1">Filtered Car Owners</h2>
+                        {
+                            this.state.loading &&
+                            (
+                                <div className="text-center p-5">
+                                    <i className="fa fa-spin fa-spinner fa-2x"></i>
+                                    <br/>
+                                    <small>Loading</small>
+                                </div>
+                            )
+                        }
+                        {
+                            !this.state.loading && this.state.car_owners.length === 0 &&
+                            (
+                                <div className="text-center p-5">
+                                    <small>No Car Owner Found Based On Chosen Filter</small>
+                                    <br/>
+                                    <Link to="/">Go Back</Link>
+                                </div>
+                            )
+                        }
+                        {
+                            this.state.car_owners.map((car_owner) => {
+                                return (
+                                    <div key={car_owner.id} className="card shadow-lg mb-3">
+                                        <div className="card-body">
+                                            <div className="row">
+                                                <div className="col-4">
+                                                    <img src={car} width="120" height="120" className="img-responsive"/>
+                                                </div>
+                                                <div className="col-8">
+                                                    <h6>{car_owner.first_name + " " + car_owner.last_name}</h6>
+                                                    <div className="">
+                                                        <ul className="p-0">
+                                                            <li className="border-right p-1">
+                                                                <p className="mb-0 text-muted c-text">Brand</p>
+                                                                <p className="mb-0 c-text">{car_owner.car_model}</p>
+                                                            </li>
+                                                            <li className="border-right p-1">
+                                                                <p className="mb-0 text-muted c-text">Year</p>
+                                                                <p className="mb-0 c-text">{car_owner.car_model_year}</p>
+                                                            </li>
+                                                            <li className="p-1 text-center">
+                                                                <p className="mb-0 text-muted c-text">Color</p>
+                                                                <p className="mb-0 color text-center" style={{margin: 'auto', backgroundColor: car_owner.car_color}}></p>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                    <div className="">
+                                                        <ul className="p-0">
+                                                            <li className="p-1">
+                                                                <p className="mb-0 text-muted c-text">Country</p>
+                                                                <p className="mb-0 c-text">{car_owner.country}</p>
+                                                            </li>
+                                                            <li className=" p-1">
+                                                                <p className="mb-0 text-muted c-text">Gender</p>
+                                                                <p className="mb-0 c-text">{car_owner.gender}</p>
+                                                            </li>
+                                                            <li className="p-1">
+                                                                <p className="mb-0 text-muted c-text">Job</p>
+                                                                <p className="mb-0 c-text bio">{car_owner.job_title}</p>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                    <div className="ml-1">
+                                                        <p className="c-text">
+                                                            <span className="text-muted">Email:</span> {car_owner.email}
+                                                        </p>
+                                                        <p className="c-text bio">
+                                                <span
+                                                    className="text-muted">Bio:</span> {car_owner.bio}
+                                                        </p>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="col-md-2  col-lg-2"></div>
+                </div>
             </div>
         )
     }
--- src/pages/Filters.jsx
+++ src/pages/Filters.jsx
@@ -27,51 +27,57 @@ export default class Filters extends Component{
     render(){
         return (
            <div className="container">
-               <h2 className="p-2"><i className="zmdi zmdi-filter-list"></i> Filter</h2>
-               {
-                   this.state.loading &&
-                   (
-                       <div className="text-center p-5">
-                           <i className="fa fa-spin fa-spinner fa-2x"></i>
-                           <br/>
-                           <small>Loading</small>
-                       </div>
-                   )
-               }
-               {
-                   this.state.filters.map((filter)=>{
-                       return (
-                           <Link key={filter.id} to={"car-owners/"+ "" +filter.id}>
-                             <div className="card shadow-lg mb-3" >
-                               <div className="card-body text-center">
-                                    <h4>{filter.start_year} - {filter.end_year} </h4>
-                                    <h6>{filter.gender}</h6>
-                                   <div>
-                                       {
-                                           filter.countries.map((country,index)=>{
-                                               return (
-                                                   <span key={index} className="badge badge-light p-2 mx-1 my-1">{country}</span>
-                                               )
-                                           })
-                                       }
-                                   </div>
-                                   <div className="mt-2">
-                                       <ul>
-                                       {
-                                           filter.colors.map((color,index)=>{
-                                               return (
-                                                   <li key={index} className="color mx-1" style={{backgroundColor: color}}></li>
-                                               )
-                                           })
-                                       }
-                                       </ul>
-                                   </div>
+               <div className="row">
+                   <div className="col-md-2  col-lg-2"></div>
+                   <div className="col-md-8  col-lg-8">
+                       <h2 className="p-2"><i className="zmdi zmdi-filter-list"></i> Filter</h2>
+                       {
+                           this.state.loading &&
+                           (
+                               <div className="text-center p-5">
+                                   <i className="fa fa-spin fa-spinner fa-2x"></i>
+                                   <br/>
+                                   <small>Loading</small>
                                </div>
-                           </div>
-                           </Link>
-                       )
-                   })
-               }
+                           )
+                       }
+                       {
+                           this.state.filters.map((filter)=>{
+                               return (
+                                   <Link key={filter.id} to={"car-owners/"+ "" +filter.id}>
+                                     <div className="card shadow-lg mb-3" >
+                                       <div className="card-body text-center">
+                                            <h4>{filter.start_year} - {filter.end_year} </h4>
+                                            <h6>{filter.gender}</h6>
+                                           <div>
+                                               {
+                                                   filter.countries.map((country,index)=>{
+                                                       return (
+                                                           <span key={index} className="badge badge-light p-2 mx-1 my-1">{country}</span>
+                                                       )
+                                                   })
+                                               }
+                                           </div>
+                                           <div className="mt-2">
+                                               <ul>
+                                               {
+                                                   filter.colors.map((color,index)=>{
+                                                       return (
+                                                           <li key={index} className="color mx-1" style={{backgroundColor: color}}></li>
+                                                       )
+                                                   })
+                                               }
+                                               </ul>
+                                           </div>
+                                       </div>
+                                   </div>
+                                   </Link>
+                               )
+                           })
+                       }
+                   </div>
+                   <div className="col-md-2  col-lg-2"></div>
+               </div>
            </div>
         )
     }
